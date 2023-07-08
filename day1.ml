0226aa171e005a41eeb9709a89f1fabf1ecb5336
TiagoSMarques <marques.tiago19@gmail.com>
2023-07-08 21:23:49 +0100

    day1 cleaner and formated

--- day1.ml
+++ day1.ml
@@ -4,31 +4,42 @@ open Readfile
 
 let inputFormat input =
   List.map input ~f:(fun x ->
-      if String.compare x "" = 0 then 0 else Int.of_string x)
+    match x with
+    | "" -> 0
+    | _ -> Int.of_string x)
+;;
+
+let sumCals l =
+  List.fold_right l ~init:[ 0 ] ~f:(fun item acc ->
+    match item with
+    | 0 -> item :: acc
+    | _ ->
+      (* add item to the the first element is results  *)
+      (match acc with
+       | [] -> []
+       | hd :: tl -> (hd + item) :: tl))
+;;
 
 let findMax l_in = List.fold l_in ~init:0 ~f:(fun acc x -> Int.max x acc)
 
-(* Another way of finding max its the same speed*)
-(* let findMax_2 l_in =
-   List.max_elt l_in ~compare:Int.compare |> function None -> 0 | Some n -> n *)
-
-let sumCals l =
-  let incFirstItem l n = match l with [] -> [] | hd :: tl -> (hd + n) :: tl in
-  List.fold_right l ~init:[ 0 ] ~f:(fun item acc ->
-      match item with 0 -> item :: acc | _ -> incFirstItem acc item)
-
 (* Part 1 *)
 let () =
-  read_lines "day1.txt" |> inputFormat |> sumCals |> findMax
-  |> printf "Day 1 p1 %i\n"
+  read_lines_2 "day1.txt" |> inputFormat |> sumCals |> findMax |> printf "Day 1 p1 %i\n"
+;;
 
 (* Part 2 *)
-let rec sum = function [] -> 0 | x :: xs -> x + sum xs
+let rec sum = function
+  | [] -> 0
+  | x :: xs -> x + sum xs
+;;
 
 let () =
   let sortedCals =
-    read_lines "day1.txt" |> inputFormat |> sumCals
+    read_lines "day1.txt"
+    |> inputFormat
+    |> sumCals
     |> List.sort ~compare:(fun x y -> compare y x)
     (* sort in descending order *)
   in
   List.take sortedCals 3 |> sum |> printf "Day 1 p2 %i\n"
+;;
--- lib/readfile.ml
+++ lib/readfile.ml
@@ -1,12 +1,17 @@
-open Stdio
+open Stdlib
 
 let read_lines filename : string list =
-  let ic = In_channel.create filename in
+  let ic = Stdio.In_channel.create filename in
   let rec read_lines_aux acc =
     match In_channel.input_line ic with
     | Some line -> read_lines_aux (line :: acc)
     | None ->
-        In_channel.close ic;
-        List.rev acc
+      In_channel.close ic;
+      List.rev acc
   in
   read_lines_aux []
+;;
+
+let read_lines_2 file =
+  In_channel.with_open_text file In_channel.input_all |> Str.(split (regexp "\n"))
+;;
