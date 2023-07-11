a72dc86fa87b3d31692924a90531922b461fa3da
TiagoSMarques <marques.tiago19@gmail.com>
2023-07-11 23:09:22 +0100

    day 3 p1

--- day3.ml
+++ day3.ml
@@ -1,6 +1,5 @@
 (* open Readfile
 
-let inp = read_lines "day3.txt"
 
 let sumPriorities l total =
   match l with
@@ -13,22 +12,31 @@ let sumPriorities l total =
     let list_matches = Seq.map (fun c -> String.index s1 c) chars_s2 in 
 ;; *)
 
+let findFirstPresentChar sack=
+
+let sack_mid = String.length sack / 2 in 
+let str = String.sub sack 0 sack_mid in 
+let s2 = String.sub sack sack_mid sack_mid in 
+let chars = List.of_seq (String.to_seq s2) in
+
+let rec aux = function
+    | [] -> '!'
+    | hd :: rest ->
+        if String.contains str hd then
+        hd
+        else
+          aux rest
+  in
+  aux chars;;
+
 String.get_int8 "Z" 0 |> print_int
 
 let hd = "helloo"
-let sack_mid = String.length hd / 2
-let s1 = String.sub hd 0 sack_mid
-let s2 = String.sub hd sack_mid sack_mid
-let chars_s2 = String.to_seq s2
 
-let list_matches =
-  Seq.exists
-    (fun c ->
-      match String.index_opt s1 c with
-      | Some _ -> true
-      | None -> false)
-    chars_s2
-;;
+let inp = read_lines "day3.txt";
+let char_list = List.map (fun (sack) -> findFirstPresentChar sack) inp;;
+
+
 
 let () = Stdio.printf "s1: %s \n" s1
 let () = Stdio.printf "s2: %s \n" s2
