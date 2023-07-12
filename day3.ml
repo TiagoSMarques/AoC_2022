48dde4301eaa6659e4005a3c11a1458b79fe40fa
TiagoSMarques <marques.tiago19@gmail.com>
2023-07-12 16:44:17 +0100

    day3 p2

--- day3.ml
+++ day3.ml
@@ -1,7 +1,7 @@
 open Readfile
 open Base
 
-let firstOcurrence sack s1' =
+let firstMatch sack s1' =
   let rec aux = function
     | [] -> None
     | hd :: rest ->
@@ -11,17 +11,6 @@ let firstOcurrence sack s1' =
         aux rest
   in
   aux sack
-;;
-
-let findFirstPresentChar sack acc' =
-  let sack_mid = String.length sack / 2 in
-  let s1 = String.sub sack ~pos:0 ~len:sack_mid in
-  let s2 = String.sub sack ~pos:sack_mid ~len:sack_mid in
-  let chars_s2 = String.to_list s2 in
-
-  match firstOcurrence chars_s2 s1 with
-  | None -> failwith "No common items in sack"
-  | Some c -> c :: acc'
 ;;
 
 let sumPriority cl =
@@ -38,11 +27,65 @@ let sumPriority cl =
   aux cl res
 ;;
 
+let findCommonItem sack acc' =
+  let sack_mid = String.length sack / 2 in
+  let s1 = String.sub sack ~pos:0 ~len:sack_mid in
+  let s2 = String.sub sack ~pos:sack_mid ~len:sack_mid in
+  let chars_s2 = String.to_list s2 in
+
+  match firstMatch chars_s2 s1 with
+  | None -> failwith "No common items in sack"
+  | Some c -> c :: acc'
+;;
+
+let inp = read_lines "day3.txt"
+
 (* part 1  *)
 let () =
-  let inp = read_lines "day3.txt" in
-  let char_list =
-    List.fold inp ~init:[] ~f:(fun acc sack -> findFirstPresentChar sack acc)
-  in
+  let char_list = List.fold inp ~init:[] ~f:(fun acc sack -> findCommonItem sack acc) in
   sumPriority char_list |> Stdio.printf "res : %d\n"
 ;;
+
+type group = {
+  s1: string;
+  s2: string;
+  s3: string;
+}
+
+let matches badge_cand sack =
+  let rec aux matched_chars = function
+    | [] -> matched_chars
+    | hd :: rest ->
+      if String.contains sack hd then
+        aux (hd :: matched_chars) rest
+      else
+        aux matched_chars rest
+  in
+  aux [] badge_cand
+;;
+
+let rec makeGroups = function
+  | [] -> []
+  | a :: b :: c :: tl -> { s1 = a; s2 = b; s3 = c } :: makeGroups tl
+  | _ -> failwith "Cant make groups of 3"
+;;
+
+(* let alternative = List.groupi inp ~break:(fun i _ _ -> i % 3 = 0) *)
+
+let findBadge group' =
+  let possible_badges = String.to_list group'.s1 in
+  match matches possible_badges group'.s2 with
+  | [] -> failwith "no common badges with s2"
+  | badge_candidates ->
+    (match matches badge_candidates group'.s3 with
+     | [badge] -> Stdio.printf "%c" badge
+     | _ -> failwith "no common badges with s3")
+;;
+
+let () =
+  (* let badges = *)
+  (* List.fold (makeGroups inp) ~init:[] ~f:(fun acc group' -> findBadge group' :: acc) *)
+  List.iter (makeGroups inp) ~f:(fun group' -> findBadge group')
+;;
+(* in
+  sumPriority badges |> Stdio.printf "res : %d\n" *)
