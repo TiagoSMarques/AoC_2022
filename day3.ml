25968d108e8fea4e68ae24d2ea9fdf8368bebf5e
TiagoSMarques <marques.tiago19@gmail.com>
2023-07-12 12:47:04 +0100

    day 3 p1 complete

--- day3.ml
+++ day3.ml
@@ -1,37 +1,48 @@
 open Readfile
+open Base
 
-let findFirstPresentChar sack l_chars =
-  let open Base in
+let firstOcurrence sack s1' =
+  let rec aux = function
+    | [] -> None
+    | hd :: rest ->
+      if String.contains s1' hd then
+        Some hd
+      else
+        aux rest
+  in
+  aux sack
+;;
+
+let findFirstPresentChar sack acc' =
   let sack_mid = String.length sack / 2 in
   let s1 = String.sub sack ~pos:0 ~len:sack_mid in
   let s2 = String.sub sack ~pos:sack_mid ~len:sack_mid in
-  let chars = String.to_list s2 in
+  let chars_s2 = String.to_list s2 in
 
-  let rec firstOcurrence = function
-    | [] -> failwith "No same items in the 2 compartments"
-    | hd :: rest ->
-      if String.contains s1 hd then
-        hd
-      else
-        firstOcurrence rest
-  in
-  firstOcurrence chars :: l_chars
+  match firstOcurrence chars_s2 s1 with
+  | None -> failwith "No common items in sack"
+  | Some c -> c :: acc'
 ;;
 
-String.get_int8 "Z" 0 |> print_int
+let sumPriority cl =
+  let res = 0 in
+  let rec aux cl res' =
+    match cl with
+    | [] -> res'
+    | hd :: tl ->
+      if Char.is_lowercase hd then
+        aux tl (res' + Char.to_int hd - 96)
+      else
+        aux tl (res' + Char.to_int hd - 38)
+  in
+  aux cl res
+;;
 
-let calPriority = List.map ()
-
-(* let hd = "helloo" *)
-
-let inp = read_lines "day3.txt"
-let char_list = List.fold_left (fun acc sack -> findFirstPresentChar sack acc) [] inp
-let () = List.iter print_char char_list
-(* 
-let () = Stdio.printf "s1: %s \n" s1
-let () = Stdio.printf "s2: %s \n" s2
-let () = Stdio.printf "\n\n %d" (Seq.length list_matches) *)
-(* let () = Seq.iter print_char chars_s2 *)
-(* let () = Seq.iter (Stdio.printf "%i \n") list_matches *)
-
-(* let () = sumPoints_1 inp 0 |> printf "Day2 p1 : %d \n" *)
+(* part 1  *)
+let () =
+  let inp = read_lines "day3.txt" in
+  let char_list =
+    List.fold inp ~init:[] ~f:(fun acc sack -> findFirstPresentChar sack acc)
+  in
+  sumPriority char_list |> Stdio.printf "res : %d\n"
+;;
