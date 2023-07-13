e8d6b03b0d6d54d2e3998c03bc0e1f9268fb1c07
TiagoSMarques <marques.tiago19@gmail.com>
2023-07-13 20:10:28 +0100

    day 4 p1

--- day4.ml
+++ day4.ml
@@ -1,19 +1,29 @@
 open Base
 
-let inp = Readfile.read_lines "day4.txt"
+let makePair str' =
+  let map_touple f ((a, b), (c, d)) = (f a, f b), (f c, f d) in
 
-(* let getAssignements file_row=  *)
-let i = "2-4,6-8"
-
-let splitAndTouple str' char' =
-  match String.split str' ~on:char' with
-  | [t1; t2] -> t1, t2
-  | []
-  | _ ->
-    failwith "No wrong format"
+  let splitAndTouple str' char' =
+    match String.split str' ~on:char' with
+    | [t1; t2] -> t1, t2
+    | []
+    | _ ->
+      failwith "Wrong format"
+  in
+  let a, b = splitAndTouple str' ',' in
+  map_touple Int.of_string (splitAndTouple a '-', splitAndTouple b '-')
 ;;
 
-let (z1x, z2x), (z1y, z2y) =
-  let a, b = splitAndTouple i ',' in
-  splitAndTouple a '-', splitAndTouple b '-'
+let checkIfContained ((z1x, z1y), (z2x, z2y)) =
+  if (z1x >= z2x && z1y <= z2y) || (z2x >= z1x && z2y <= z1y) then
+    1
+  else
+    0
+;;
+
+(* part 1 *)
+let () =
+  let inp = Readfile.read_lines "day4.txt" in
+  List.fold inp ~init:0 ~f:(fun acc pair' -> (makePair pair' |> checkIfContained) + acc)
+  |> Stdio.printf "res : %d\n"
 ;;
