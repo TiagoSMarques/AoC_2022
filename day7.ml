33ee735186d837a5431ba8af15429c4ac467b41c
TiagoSMarques <marques.tiago19@gmail.com>
2023-07-30 11:24:52 +0100

    day 7

--- day7.ml
+++ day7.ml
@@ -2,13 +2,10 @@ open Base
 
 type fileTree =
   | File of (string * int)
-  | Dir of {
-      name: string;
-      contents: fileTree list;
-    }
+  | Dir of (string * fileTree list)
 
 let appendFiles file = function
-  | Dir { name = n; contents = c } -> Dir { name = n; contents = file :: c }
+  | Dir (n, c) -> Dir (n, file :: c)
   | File _ -> failwith "dir must be a folder"
 ;;
 
@@ -41,25 +38,34 @@ let buildFileTree inp =
   aux inp 0
 ;;
 
-(* let example_dir =
-   Dir [File ("1", 1); File ("2", 2); Dir [File ("3", 3); File ("4", 4)]; File ("5", 5)]
-   ;; *)
+let dictionary = ["a", [File ("1", 1); Dir ("b", [])]; "banana", [File ("3", 3)]]
+let updated_dictionary = List.Assoc.add dictionary ~equal:String.equal "a"
+let ex4 = Dir ("/", [File ("1", 1); Dir ("a", [File ("2", 2)])])
+
+let app_at_depth dir target_depth name =
+  let rec aux depth folder =
+    match folder with
+    | Dir (_, c) ->
+      if depth < target_depth then
+        aux (depth + 1) c
+      else
+        c
+    | File _ -> failwith "dir must be a folder"
+  in
+  aux 0 dir
+;;
 
 let inp = Readfile.read_lines "day7.txt"
 let a = buildFileTree inp
-let ex1 = Dir { name = "aaa"; contents = [File ("1", 1); File ("2", 2); File ("3", 3)] }
-let ex2 = Dir { name = "/"; contents = [] }
-
-let ex3 =
-  appendFiles (File ("1", 1)) ex2 |> appendFiles (Dir { name = "a"; contents = [] })
-;;
+let ex2 = Dir ("/", [])
+let ex3 = appendFiles (File ("1", 1)) ex2 |> appendFiles (Dir ("a", []))
 
 let sumList file_tree =
   let sum_lst = ref [] in
   let rec aux = function
     | File (_, size) -> size
-    | Dir n ->
-      let total = List.fold n.contents ~init:0 ~f:(fun acc elm -> aux elm + acc) in
+    | Dir (_, n) ->
+      let total = List.fold n ~init:0 ~f:(fun acc elm -> aux elm + acc) in
       sum_lst := total :: !sum_lst;
       total
   in
@@ -67,11 +73,15 @@ let sumList file_tree =
   !sum_lst
 ;;
 
+(* let example_dir =
+   Dir [File ("1", 1); File ("2", 2); Dir [File ("3", 3); File ("4", 4)]; File ("5", 5)]
+   ;; *)
+
 (* let ex2 = Dir [Dir [File ("1", 1)]; Dir [File ("2", 2)]]
    let ex3 = Dir [File ("1", 1); Dir [File ("2", 2)]]
    let ex4 = Dir [Dir [File ("1", 1); Dir [File ("5", 5)]]; Dir [File ("3", 3)]] *)
 (* let a = sumList example_dir;; *)
 
-let b = sumList ex1;;
+let b = sumList ex2;;
 
 Readfile.print_listof_ints b
