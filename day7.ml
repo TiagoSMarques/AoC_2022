0b051829f6a4a84253ea7724d9fd8f7926e56c30
TiagoSMarques <marques.tiago19@gmail.com>
2023-08-03 22:19:07 +0100

    day 7 part 1

--- day7.ml
+++ day7.ml
@@ -2,65 +2,55 @@ open Base
 
 type fileTree =
   | File of (string * int)
-  | Dir of (string * fileTree list)
+  | Dir of {
+      name: string;
+      mutable files: fileTree list;
+    }
 
-let appendFiles file = function
-  | Dir (n, c) -> Dir (n, file :: c)
-  | File _ -> failwith "dir must be a folder"
-;;
-
-let remove_first = function
+let rec remove_last lst =
+  match lst with
   | [] -> []
-  | _ :: tl -> tl
+  | [_] -> []
+  | hd :: tl -> hd :: remove_last tl
 ;;
 
-(* Format the input to build the fileTree*)
-let rec findAndAppend dir path line =
-  let rec aux dir path comp_dir =
+(* traveses the path until the last folder and appends the line to it *)
+let findAndAppend dir path line =
+  let rec aux dir path =
     match dir with
-    | [] -> failwith ("No dir found" ^ List.hd_exn path)
-    | Dir (n, c) :: _ when String.equal n (List.hd_exn path) ->
+    | [] -> failwith ("No dir found: " ^ List.hd_exn path)
+    | Dir fs :: _ when String.equal fs.name (List.hd_exn path) ->
       if List.length path <= 1 then
-        Dir (n, line :: c)
+        (* Stdio.printf "folder: %s " fs.name; *)
+        fs.files <- line :: fs.files
       else
-        aux c (remove_first path) (Dir (n, c) :: comp_dir)
-    | _ :: tl -> aux tl path comp_dir
+        aux fs.files (List.tl_exn path)
+    | _ :: tl -> aux tl path
   in
-  aux dir path []
+  aux [dir] path;
+  dir
 ;;
 
 let buildFileTree inp =
-  let rec aux inp depth =
+  let rec aux inp fs path =
     match inp with
-    | [] -> failwith "done"
+    | [] -> fs
     | hd :: tl ->
       (match String.split hd ~on:' ' with
-       | ["$"; "cd"; ".."] ->
-         Stdio.printf "Up depth %d\n" (depth - 1);
-         aux tl (depth - 1)
-       | ["$"; "cd"; folder_name] ->
-         Stdio.printf "Create folder: %s \n" folder_name;
-         aux tl depth
-       | ["$"; "ls"] ->
-         Stdio.printf "List depth %d \n" (depth + 1);
-         aux tl (depth + 1)
+       | ["$"; "cd"; "/"] -> aux tl fs ["/"]
+       | ["$"; "cd"; ".."] -> aux tl fs (remove_last path)
+       | ["$"; "cd"; folder_name] -> aux tl fs (path @ [folder_name])
+       | ["$"; "ls"] -> aux tl fs path
        | ["dir"; dir_name] ->
-         Stdio.printf "Create folder: %s \n" dir_name;
-         aux tl depth
+         aux tl (findAndAppend fs path (Dir { name = dir_name; files = [] })) path
        | [size; file_name] ->
-         Stdio.printf "File: %s Size:%s \n" file_name size;
-         aux tl depth
+         aux tl (findAndAppend fs path (File (file_name, Int.of_string size))) path
        | _ -> failwith "bad format of input")
   in
 
-  aux inp 0
+  aux inp (Dir { name = "/"; files = [] }) []
 ;;
 
-let ex4 = [Dir ("/", [File ("1", 1); Dir ("a", [File ("2", 2)])])]
-let ex2 = Dir ("/", [])
-let path = ["/"; "a"]
-let c = findAndAppend ex4 path (File ("3", 3))
-let ex3 = appendFiles (File ("1", 1)) ex2 |> appendFiles (Dir ("a", []))
 let inp = Readfile.read_lines "day7.txt"
 let a = buildFileTree inp
 
@@ -68,8 +58,8 @@ let sumList file_tree =
   let sum_lst = ref [] in
   let rec aux = function
     | File (_, size) -> size
-    | Dir (_, n) ->
-      let total = List.fold n ~init:0 ~f:(fun acc elm -> aux elm + acc) in
+    | Dir fs ->
+      let total = List.fold fs.files ~init:0 ~f:(fun acc elm -> aux elm + acc) in
       sum_lst := total :: !sum_lst;
       total
   in
@@ -77,15 +67,15 @@ let sumList file_tree =
   !sum_lst
 ;;
 
-(* let example_dir =
-   Dir [File ("1", 1); File ("2", 2); Dir [File ("3", 3); File ("4", 4)]; File ("5", 5)]
-   ;; *)
+let b = sumList a
 
-(* let ex2 = Dir [Dir [File ("1", 1)]; Dir [File ("2", 2)]]
-   let ex3 = Dir [File ("1", 1); Dir [File ("2", 2)]]
-   let ex4 = Dir [Dir [File ("1", 1); Dir [File ("5", 5)]]; Dir [File ("3", 3)]] *)
-(* let a = sumList example_dir;; *)
+let part1 =
+  List.fold b ~init:0 ~f:(fun acc x ->
+    if x <= 100_000 then
+      x + acc
+    else
+      acc)
+;;
 
-let b = sumList ex2;;
-
-Readfile.print_listof_ints b
+(* Readfile.print_listof_ints b; *)
+Stdio.printf "\n Part 1: %d" part1
