7af580ffe0c621f81c8e4ea03fb88b1d31b6920b
TiagoSMarques <marques.tiago19@gmail.com>
2023-08-19 22:19:48 +0100

    d11

--- day11.ml
+++ day11.ml
@@ -1,48 +1,49 @@
 open Base
-open Re
 
 type monkey = {
   id: int;
-  items: int list;
+  items: string list;
   op: string list;
-  testT: string;
-  testF: string;
+  test: string * string * string;
 }
 
 let a =
-  "Monkey 0:
-Starting items: 79, 98
-Operation: new = old * 19
-Test: divisible by 23
-  If true: throw to monkey 2
-  If false: throw to monkey 3"
+  "Monkey 0: 
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3"
 ;;
-
-let b = Array.of_list @@ String.split ~on:'\n' a
-let l1 = b.(0)
-let id = Int.of_string @@ List.nth_exn (String.split_on_chars ~on:[' '; ':'] l1) 1
-let l2 = b.(1)
-let matches = Re.exec (Re.Posix.compile_pat "([0-9]+), ([0-9]+)") l2
-
-let id =
-  [Int.of_string @@ Re.Group.get matches 1; Int.of_string @@ Re.Group.get matches 2]
-;;
-
-let l3 = b.(2)
-let matches = Re.exec (Re.Perl.compile_pat "(?=w)[a-zA-Z0-9_]+") l3
-let op = List.tl_exn @@ String.split ~on:'=' l3
-
-(*
-    
-*)
-let b = Readfile.read_paragraph "day11.txt"
-let _ = Stdio.printf "%s" (List.hd_exn b)
 
 let parse m_string =
-  match Str.(split (regexp "\n")) m_string with
-  | [l1; l2; l3; l4; l5; l6] -> 2
+  match String.split ~on:'\n' m_string with
+  | [l1; l2; l3; l4; l5; l6] ->
+    let id = Int.of_string @@ List.nth_exn (String.split_on_chars ~on:[' '; ':'] l1) 1 in
+    let items =
+      List.filter_mapi ~f:(fun i a ->
+        match i with
+        | 4 | 6 -> Some a
+        | _ -> None)
+      @@ String.split_on_chars ~on:[' '; ','] l2
+    in
+    let op =
+      List.filteri ~f:(fun i _ -> i = 6 || i = 7 || i = 8)
+      @@ String.split_on_chars ~on:['='; ' '] l3
+    in
+    let test =
+      ( List.nth_exn (String.split ~on:' ' l4) 5,
+        List.nth_exn (String.split ~on:' ' l5) 9,
+        List.nth_exn (String.split ~on:' ' l6) 9 )
+    in
+    { id; items; op; test }
   | _ -> failwith "Bad input"
 ;;
 
-(* Parse the input for each monkey *)
+(* let p = parse a *)
+
+let board = List.map ~f:(fun a -> parse a) (Readfile.read_paragraph "day11.txt")
+(* Parse the input for each monkey - done *)
 (* Build monkey list using List.map on the input *)
+(* make the play function? *)
+(* build the game board function that updates the monkey items base on the play *)
